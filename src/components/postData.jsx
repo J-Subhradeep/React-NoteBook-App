e9fe6d9c2c26151fe72f43fc2f1e186a245fa991
ani <subhradeeppal30@gmail.com>
2021-12-30 14:59:34 +0530

    all emoji keyboard bugs fixed

--- src/components/postData.jsx
+++ src/components/postData.jsx
@@ -7,98 +7,115 @@ import Picker from "emoji-picker-react";
 import IconButton from "@mui/material/IconButton";
 import InsertEmoticonIcon from "@mui/icons-material/InsertEmoticon";
 const PostData = () => {
-  const [chosenEmoji, setChosenEmoji] = useState(false);
-  const [description, setDescription] = useState("");
-  const [title, setTitle] = useState("");
-  const onEmojiClick = (event, emojiObject) => {
-    setDescription((prev) => {
-      return description + emojiObject.emoji;
-    });
-  };
+	const [chosenEmoji, setChosenEmoji] = useState(false);
+	const [item, setItem] = useState();
+	const [description, setDescription] = useState("");
+	const [title, setTitle] = useState("");
+	const onEmojiClick = (event, emojiObject) => {
+		var now = document.activeElement;
+		console.log(now);
+		console.log(event);
+		if (item === "title") {
+			setTitle((prev) => prev + emojiObject.emoji);
+		}
+		if (item === "description") {
+			setDescription((prev) => {
+				return prev + emojiObject.emoji;
+			});
+		}
+	};
 
-  return (
-    <>
-      <Navbar />
-      <div className=" mainpostdatabody">
-        <div className="send-data-form">
-          <form action="">
-            <div className="container">
-              <div className="title-area">
-                <TextField
-                  required
-                  id="outlined-required"
-                  label="Title"
-                  value={title}
-                  onChange={(e) => {
-                    setTitle(e.target.value);
-                  }}
-                  // defaultValue="Hello World"
-                />{" "}
-                <TextField
-                  required
-                  id="outlined-required"
-                  label="User Name"
-                  defaultValue="Hello World"
-                />
-              </div>
+	return (
+		<>
+			<Navbar />
+			<div className=" mainpostdatabody">
+				<div className="send-data-form">
+					<form action="">
+						<div className="container p-3">
+							<div className="title-area">
+								<TextField
+									required
+									id="outlined-required"
+									label="Title"
+									value={title}
+									onFocus={() => {
+										setItem("title");
+									}}
+									onChange={(e) => {
+										setTitle(e.target.value);
+									}}
+									defaultValue=" "
+								/>{" "}
+								<TextField
+									required
+									id="outlined-required"
+									label="User Name"
+									defaultValue="Hello World"
+								/>
+							</div>
 
-              <TextField
-                id="filled-multiline-static"
-                label="Description"
-                multiline
-                value={description}
-                onChange={(e) => {
-                  setDescription(e.target.value);
-                }}
-                minRows={11}
-                // defaultValue="Default Value"
-                variant="filled"
-                style={{ width: "100%", marginBottom: "10px" }}
-              />
+							<TextField
+								id="filled-multiline-static"
+								label="Description"
+								multiline
+								value={description}
+								onChange={(e) => {
+									setDescription(e.target.value);
+								}}
+								onFocus={() => {
+									setItem("description");
+								}}
+								minRows={11}
+								// defaultValue="Default Value"
+								variant="filled"
+								style={{ width: "100%", marginBottom: "10px" }}
+							/>
 
-              <div className="submitpostbutton" style={{ textAlign: "right" }}>
-                <IconButton aria-label="delete" size="small">
-                  <InsertEmoticonIcon
-                    fontSize="small"
-                    onClick={() => {
-                      setChosenEmoji((prev) => !prev);
-                    }}
-                  />
-                </IconButton>
-                <Button
-                  variant="contained"
-                  endIcon={<SaveIcon />}
-                  style={{ position: "relative", right: "0" }}
-                >
-                  Save
-                </Button>
-              </div>
-            </div>
-          </form>
-        </div>
+							<div className="submitpostbutton" style={{ textAlign: "right" }}>
+								<IconButton aria-label="delete" size="small">
+									<InsertEmoticonIcon
+										fontSize="small"
+										onClick={() => {
+											setChosenEmoji((prev) => !prev);
+											setItem("description");
+										}}
+									/>
+								</IconButton>
+								<Button
+									type="submit"
+									variant="contained"
+									endIcon={<SaveIcon />}
+									style={{ position: "relative", right: "0" }}
+								>
+									Save
+								</Button>
+							</div>
+						</div>
+					</form>
+				</div>
 
-        {chosenEmoji ? (
-          <div
-            className="emojiselector"
-            style={{
-              position: "absolute",
-              border: "none",
-              borderRadius: "5px",
-              padding: "5px",
-              backgroundColor: "#1976d2",
-            }}
-          >
-            <Picker
-              onEmojiClick={onEmojiClick}
-              pickerStyle={{ boxShadow: "none" }}
-            />
-          </div>
-        ) : (
-          <></>
-        )}
-      </div>
-    </>
-  );
+				{chosenEmoji ? (
+					<div
+						className="emojiselector"
+						style={{
+							position: "absolute",
+							border: "none",
+							borderRadius: "5px",
+							padding: "5px",
+							backgroundColor: "#1976d2",
+						}}
+					>
+						<Picker
+							onEmojiClick={onEmojiClick}
+							pickerStyle={{ boxShadow: "none", backgroundColor: "white" }}
+						/>
+					</div>
+				) : (
+					<></>
+				)}
+			</div>
+		</>
+	);
 };
 
 export default PostData;
